aeb6bc51e32955042d78163e3dfdd8a5148a943e
Boris Garbuzov <b.garbuzov.vironit@vironit.ru>
2021-07-28 11:17:08 +0300

    project started, dependencies and plugins added, added connection to Firebase

---
+++ app/src/main/java/com/vironit/garbuzov_cryptocurrency/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vironit.garbuzov_cryptocurrency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

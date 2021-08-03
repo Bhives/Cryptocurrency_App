6e03a860b479b873561aa2a87fe1b1c3c69cbbbe
Boris Garbuzov <b.garbuzov.vironit@vironit.ru>
2021-08-03 13:51:10 +0300

    edited converter logic

--- app/src/main/java/com/vironit/garbuzov_cryptocurrency/ui/fragments/CryptoCurrencyConverterFragment.kt
+++ app/src/main/java/com/vironit/garbuzov_cryptocurrency/ui/fragments/CryptoCurrencyConverterFragment.kt
@@ -3,6 +3,8 @@ package com.vironit.garbuzov_cryptocurrency.ui.fragments
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -26,29 +28,136 @@ class CryptoCurrencyConverterFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         rateDateTextView.text = rateDateTextView.text.toString() + viewModel.getCurrentDate()
+        displayCurrencyRate()
+        cryptoCurrencyInput.addTextChangedListener {
+            if (!currencyInput.hasFocus()) {
+                if (cryptoCurrencyInput.text.isEmpty()) {
+                    currencyInput.text.clear()
+                }
+                convertFromCryptoCurrency()
+            }
+        }
+        cryptoCurrencyTypesSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    displayCurrencyRate()
+                    if (!currencyInput.hasFocus()) {
+                        convertFromCryptoCurrency()
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+        currencyInput.addTextChangedListener {
+            if (!cryptoCurrencyInput.hasFocus()) {
+                if (currencyInput.text.isEmpty()) {
+                    cryptoCurrencyInput.text.clear()
+                }
+                convertToCryptoCurrency()
+            }
+        }
+        currencyTypesSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    displayCurrencyRate()
+                    if (!cryptoCurrencyInput.hasFocus()) {
+                        convertToCryptoCurrency()
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun displayCurrencyRate() {
         lifecycleScope.launch {
-            convertCryptoCurrency()
+            with(cryptoCurrencyTypesSpinner.selectedItem) {
+                rateValueTextView.text = "1 $this = ${
+                    String.format(
+                        "%.2f", viewModel.convertCryptoCurrency(
+                            1.0,
+                            this.toString(),
+                            "USD"
+                        )
+                    )
+                }$"
+            }
         }
     }
 
-    private suspend fun convertCryptoCurrency() {
-        cryptoCurrencyInput.addTextChangedListener {
-            lifecycleScope.launch {
-                with(cryptoCurrencyTypesSpinner.selectedItem) {
-                    try {
-                        currencyInput.setText(
+    private fun convertFromCryptoCurrency() {
+        lifecycleScope.launch {
+            with(cryptoCurrencyTypesSpinner.selectedItem) {
+                try {
+                    currencyInput.setText(
+                        String.format(
+                            "%.2f",
                             viewModel.convertCryptoCurrency(
                                 cryptoCurrencyInput.text.toString().toDouble(),
-                                this.toString()
-                            ).toString()
+                                this.toString(),
+                                currencyTypesSpinner.selectedItem.toString()
+                            )
                         )
-                    } catch (iOException: IOException) {
-                        iOException.printStackTrace()
-                    } catch (httpException: HttpException) {
-                        httpException.printStackTrace()
-                    } catch (numberFormatException: NumberFormatException) {
-                        numberFormatException.printStackTrace()
-                    }
+                    )
+                } catch (iOException: IOException) {
+                    iOException.printStackTrace()
+                } catch (httpException: HttpException) {
+                    httpException.printStackTrace()
+                } catch (numberFormatException: NumberFormatException) {
+                    numberFormatException.printStackTrace()
+                } catch (noSuchElementException: NoSuchElementException) {
+                    Toast.makeText(
+                        context,
+                        "No rate was found for the ${currencyTypesSpinner.selectedItem} currency",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    noSuchElementException.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun convertToCryptoCurrency() {
+        lifecycleScope.launch {
+            with(currencyTypesSpinner.selectedItem) {
+                try {
+                    cryptoCurrencyInput.setText(
+                        String.format(
+                            "%.2f",
+                            (currencyInput.text.toString()
+                                .toDouble() / viewModel.convertCryptoCurrency(
+                                1.0,
+                                cryptoCurrencyTypesSpinner.selectedItem.toString(),
+                                this.toString()
+                            ))
+                        )
+                    )
+                } catch (iOException: IOException) {
+                    iOException.printStackTrace()
+                } catch (httpException: HttpException) {
+                    httpException.printStackTrace()
+                } catch (numberFormatException: NumberFormatException) {
+                    numberFormatException.printStackTrace()
+                } catch (noSuchElementException: NoSuchElementException) {
+                    Toast.makeText(
+                        context,
+                        "No rate was found for the ${currencyTypesSpinner.selectedItem} currency",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    noSuchElementException.printStackTrace()
                 }
             }
         }
--- app/src/main/java/com/vironit/garbuzov_cryptocurrency/viewmodels/CryptoCurrencyConverterViewModel.kt
+++ app/src/main/java/com/vironit/garbuzov_cryptocurrency/viewmodels/CryptoCurrencyConverterViewModel.kt
@@ -3,7 +3,6 @@ package com.vironit.garbuzov_cryptocurrency.viewmodels
 import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
 import com.vironit.garbuzov_cryptocurrency.data.CryptoCurrencyRepository
-import com.vironit.garbuzov_cryptocurrency.data.entities.CryptoCurrency
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,8 +10,8 @@ import javax.inject.Inject
 class CryptoCurrencyConverterViewModel @Inject constructor(var cryptoCurrencyRepository: CryptoCurrencyRepository) :
     ViewModel() {
 
-    suspend fun convertCryptoCurrency(amount: Double, symbol: String): Double {
-        return cryptoCurrencyRepository.getCurrentCryptoCurrency(amount, symbol).quote.getValue("USD").price
+    suspend fun convertCryptoCurrency(amount: Double, cryptoCurrencySymbol: String, currencySymbol: String): Double {
+        return cryptoCurrencyRepository.getCurrentCryptoCurrency(amount, cryptoCurrencySymbol).quote.getValue(currencySymbol).price
     }
 
     @SuppressLint("NewApi")
